c074ac751c902c1271d337333a07e4d47aa131e0
Seungyeon Oh <alstmd9902@naver.com>
2026-01-04 17:21:14 +0900

    feat: React Router로 admin 페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Users from './pages/admin__users/Users';
 import UserDetail from './pages/admin__users/UserDetail';
 import Settings from './pages/admin__settings/Settings';
 import NotFound from './pages/admin__notfound/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   //1. React Router의 `BrowserRouter`를 설정해야 합니다.
@@ -16,7 +17,26 @@ function App() {
   //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
   //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
 
-  return;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            {/* 메인 페이지 */}
+            <Route path="/admin" element={<Home />} />
+            {/* admin 영역 */}
+            <Route path="/admin">
+              <Route index element={<Dashboard />} />
+              <Route path="/admin/users" element={<Users />} />
+              <Route path="/admin/users/:id" element={<UserDetail />} />
+              <Route path="/admin/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
